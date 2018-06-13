d4447e8f34cbc4aa00e4c060c31a2a9b68c22065
keerthivasand <keerthivasan9491@gmail.com>
2018-06-13 23:55:52 +0530

    chnage

--- 2018/first_step/contact/script.js
+++ 2018/first_step/contact/script.js
@@ -6,31 +6,30 @@ function producePrompt(message,err_id,color){
     document.getElementById(err_id).innerHTML = message;
     document.getElementById(err_id).style.color = color;
 }
-
-
-
-
 function validateForm(){
     var name_usr = document.getElementById('contact-name').value;
     var phone_no = document.getElementById('contact-phone').value;
     var email_id = document.getElementById('contact-email').value;
     var message = document.getElementById('contact-message').value;
+    var charAZ = /^[a-zA-Z\s]+$/;
+    var phoneno = /^\d{10}$/;
+    var emailval = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-    if(name_usr == ''){
-        producePrompt('please enter name', 'name-error','red');
+    if(name_usr == '' || !name_usr.match(charAZ) ){
+        producePrompt('Please enter valid name. Your name should contain only A-Z Letters', 'name-error','red');
         return false;
     }
     else{
-        producePrompt('Good Name', 'name-error','green');
+        producePrompt('Good Name', 'name-error','green');        
     }
-    if(phone_no == ''){
-        producePrompt('please enter phone', 'phone-error','red');
+    if(phone_no == '' || !phone_no.match(phoneno)){
+        producePrompt('please enter valid phone', 'phone-error','red');
         return false;
     }
     else{
-        producePrompt('Valid', 'phone-error','green');
+        producePrompt('Valid', 'phone-error','green');        
     }
-    if(email_id == ''){
+    if(email_id == '' || !email_id.match(emailval)){
         producePrompt('please enter email', 'email-error','red');
         return false;
     }
@@ -44,6 +43,7 @@ function validateForm(){
     else{
         producePrompt('Thanks', 'message-error','green');
     }
+    alert("Your Form Submitted");
     return true;
 }
 
